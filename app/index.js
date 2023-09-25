3901eb1711ab852e4bd3e9fa7053a7493340afb2
nagendra04 <nagendra.sardar@gmail.com>
2023-09-25 14:57:05 +0545

    New Text

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+
+const Home =()=>{
+    return (
+
+        <View><Text>Hello World This is Me: Nagendra Sardar.</Text></View>
+    );
+}
+
+export default Home;
